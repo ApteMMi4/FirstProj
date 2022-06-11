f5fad43bfc623d4a2ee8b53278ebfcdae39ea018
aptemmch <apremmch@gmail.com>
2022-06-11 16:29:15 +0300

    new newm

---
+++ app/Models/Conclusions.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Conclusions extends Model
+{
+    use HasFactory;
+
+    protected $table = 'conclusions';
+
+    protected $fillable = [
+        'valute',
+        'return_valute',
+        'return',
+    ];
+
+    public function getUserAttribute()
+    {
+        $user = User::find($this->user_id);
+        return $user;
+    }
+}
