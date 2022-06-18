836283e17846070f7c00eac5559cf06b9af670f8
aptemmch <apremmch@gmail.com>
2022-06-18 22:48:52 +0300

    new newm

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -23,7 +23,6 @@ class AdminController extends Controller
             'gateway'=> 'default',
             'withdraw_to'=> '4111111111111111' //$conslusions->return
         ];
-dd($request_data);
 
         $body_string_arr = [];
 
