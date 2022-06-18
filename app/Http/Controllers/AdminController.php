d43877337358226ed20aa5c8f0ba2021ae694c0d
aptemmch <apremmch@gmail.com>
2022-06-18 22:48:03 +0300

    new newm

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -19,7 +19,7 @@ class AdminController extends Controller
 
         $request_data['request_data'] = [
             'withdraw_type' => 'UAH',
-            'amount' => float($conslusions->sum),
+            'amount' => floor($conslusions->sum),
             'gateway'=> 'default',
             'withdraw_to'=> '4111111111111111' //$conslusions->return
         ];
