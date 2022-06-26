6e9a82d6844d4f7f4c779ee928e445eebca6fa81
apremmch <apremmch@gmail.com>
2022-06-26 12:28:45 +0300

    qq

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -64,11 +64,11 @@ class AdminController extends Controller
         $res_array = json_decode($res, TRUE);
 
         if(isset($res_array[0]['withdrawal_id']) && $res_array[0]['status']=='awaiting_confirmation'){
-            $conslusions->status = 'success';
+            $conslusions->status = 'new';
             $conslusions->withdrawal_id = $res_array[0]['withdrawal_id'];
 
 
-            $transMass = ['total'=>(floor($conslusions->sum)*-1), 'currency'=>'UAH', 'status'=>'new','shop_id'=>$conslusions->user_id];
+            $transMass = ['total'=>(floor($conslusions->sum)*-1), 'currency'=>'uah', 'status'=>'new','shop_id'=>$conslusions->user_id];
             Transactions::create($transMass);
             $conslusions->save();
         }
